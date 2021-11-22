e2bee7b3306d0a5767a94f1fc75521298c0eb36e
evboggs302 <evboggs94@gmail.com>
2021-11-22 16:49:25 -0500

    getting ready for AWS

--- server/index.js
+++ server/index.js
@@ -183,7 +183,6 @@ const path = require("path");
 //   res.sendFile(path.join(__dirname, "/../build/index.html"));
 // });
 app.use(express.static(path.join(__dirname, "build")));
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
