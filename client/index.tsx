bd5c71bcedf94086eb3808e2dd0b08e20395e208
Evan Boggs <49161965+evboggs302@users.noreply.github.com>
2024-03-31 13:08:11 -0400

    testing-pr (#79)
    * testing-pr
    
    * cleanup

--- client/index.tsx
+++ client/index.tsx
@@ -4,7 +4,6 @@ import { ApolloClientProvider } from "./apolloClient/apolloClient";
 import { AppRouter } from "./routes/appRouter";
 import { TokenContextProvider } from "./src/lib/context/tokenContext/TokenContext";
 import { StyleProvider } from "@ant-design/cssinjs";
-// import { ToastContextProvider } from "./src/shared/context/toastContext/ToastContext";
 import { AntGlobalsWrapper } from "./src/lib/context/antGlobals/AntGlobalsWrapper";
 
 createRoot(document.getElementById("root")!).render(
