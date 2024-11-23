3a41206feb160fc82045a41446ab0853fccd18cf
Evan Boggs <49161965+evboggs302@users.noreply.github.com>
2024-11-22 21:19:32 -0500

    why (#104)

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloClientProvider } from "./src/apolloClient/apolloClient";
-import { AppRouter } from "./src/routes/AppRouter";
+import { AppRouter } from "./src/routes/ApplicationRouter";
 import { TokenContextProvider } from "./src/lib/context/TokenContext";
 
 createRoot(document.getElementById("root")!).render(
