f1c0e1460a37a736cfd29e7c8b29b2d279aee8c0
evboggs302 <evboggs94@gmail.com>
2024-12-06 17:07:44 -0500

    selected tab value

--- client/src/features/hunts/components/HuntPage.tsx
+++ client/src/features/hunts/components/HuntPage.tsx
@@ -1,7 +1,7 @@
 import React, { SyntheticEvent, useCallback, useState } from "react";
 import { useHuntContext } from "@lib/context/HuntContext";
 import { ClueQryContextProvider } from "@lib/context/ClueContext";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import Tabs from "@mui/material/Tabs";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
@@ -9,7 +9,18 @@ import Tab from "@mui/material/Tab";
 export const HuntPage = () => {
   // const { loading, data } = useHuntContext();
   const navigate = useNavigate();
-  const [location, setLocation] = useState("");
+  const { pathname } = useLocation();
+
+  let location: "" | "clues" | "teams" | "responses";
+  if (pathname.includes("clues")) {
+    location = "clues";
+  } else if (pathname.includes("teams")) {
+    location = "teams";
+  } else if (pathname.includes("responses")) {
+    location = "responses";
+  } else {
+    location = "";
+  }
 
   // if (!data?.hunt) {
   //   return null;
@@ -27,7 +38,6 @@ export const HuntPage = () => {
   const handleChange = useCallback(
     (event: SyntheticEvent, value: string) => {
       event?.preventDefault();
-      setLocation(value);
       navigate(value, { relative: "path" });
     },
     [navigate]
