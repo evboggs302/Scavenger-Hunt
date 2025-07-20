0557f31dc99aad6d67aa7ef42146ab988186a696
evboggs302 <evboggs94@gmail.com>
2025-07-20 07:42:59 -0400

    messing-with-pw-running-ci-2

--- client/playwright/e2eTest.ts
+++ client/playwright/e2eTest.ts
@@ -65,8 +65,9 @@ export const e2eTest = testBase.extend<Fixtures, WorkerOptions>({
       await page.getByTestId("login-password").fill(variables.password);
       await page.getByTestId("login-submit").click();
 
+      // The login flow sets localStorage state in the process and redirects.
+      // Wait for the final URL to ensure that the localStorage values are actually set.
       await page.waitForURL(`${process.env.CLIENT_URL}/app`);
-      // Alternatively, you can wait until the page reaches a state where all cookies are set.
       await expect(
         page.getByRole("button", { name: "New hunt" })
       ).toBeVisible();
