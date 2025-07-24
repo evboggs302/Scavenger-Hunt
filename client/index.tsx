143875b3d2f6a9497efd561fce9ab469fe85114d
Evan Boggs <49161965+evboggs302@users.noreply.github.com>
2025-07-24 15:11:45 -0400

    sentry-possibly-maybe (#188)

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,21 @@
+import * as Sentry from "@sentry/react";
 import { createRoot } from "react-dom/client";
 import { ApolloClientProvider } from "@apolloClient/apolloClient";
 import { AppRouter } from "@routes/AppRouter";
 import { TokenContextProvider } from "@lib/context/TokenContext";
 import { AppMUIProviders } from "@lib/context/AppMUIProviders";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: window.location.hostname,
+  beforeSend(event) {
+    if (window.location.hostname === "localhost") {
+      // Don't send events in development
+      return null;
+    }
+    return event;
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <ApolloClientProvider>
