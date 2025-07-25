4f6fb337290e30170056ea0f01f80c134a0069d0
evboggs302 <evboggs94@gmail.com>
2025-07-25 16:14:25 -0400

    diagram-ci-test-updates

--- client/src/lib/hooks/useScrollShadow.test.tsx
+++ client/src/lib/hooks/useScrollShadow.test.tsx
@@ -1,45 +1,40 @@
-import { vi } from "vitest";
-import { render, renderHook, screen } from "@testing-library/react";
-import { bottomShadow, useScrollShadow } from "./useScrollShadow";
-import Box from "@mui/material/Box";
 import { forwardRef } from "react";
+import { render, renderHook, screen, waitFor } from "@testing-library/react";
+import Box from "@mui/material/Box";
+import { useScrollShadow } from "./useScrollShadow";
 
-global.ResizeObserver = vi.fn().mockImplementation((callback) => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn(),
-  callback,
-}));
-
-const DummyComponent = forwardRef((props, ref) => (
-  <Box
-    ref={ref}
-    data-testid="dummy-div"
-    style={{ height: 50, overflowY: "auto" }}
-  />
-));
-
-const DummyComponentWithChildren = forwardRef((props, ref) => (
+const DummyComponent = forwardRef<HTMLElement>((_, ref) => (
   <Box
     ref={ref}
     data-testid="dummy-div"
     style={{
-      height: 50,
+      height: "50px",
       display: "flex",
       flexDirection: "column",
       overflowY: "auto",
     }}
   >
-    {[...Array(25)].map((_, index) => (
-      <div style={{ height: 25 }} key={index}>
-        {index}
-      </div>
-    ))}
+    <div style={{ height: 25 }}>BLAH</div>
   </Box>
 ));
 
 describe("useScrollShadow", () => {
-  it("default state", () => {
+  vi.stubGlobal(
+    "ResizeObserver",
+    class MockedResizeObserver {
+      constructor(cb: ResizeObserverCallback) {
+        setTimeout(() => {
+          cb([], this);
+        }, 100);
+      }
+
+      observe = vi.fn();
+      unobserve = vi.fn();
+      disconnect = vi.fn();
+    }
+  );
+
+  it("hook default state", async () => {
     const { result } = renderHook(() => useScrollShadow());
     const { hasOverflow, ref } = result.current;
 
@@ -47,53 +42,16 @@ describe("useScrollShadow", () => {
     expect(ref).toEqual({ current: null });
   });
 
-  it("applied to an element", () => {
+  it("REF applied to an element", async () => {
     const { result } = renderHook(() => useScrollShadow());
     const { ref } = result.current;
 
-    render(<DummyComponent ref={ref} />);
+    await waitFor(() => render(<DummyComponent ref={ref} />));
+    const scrollableDiv = screen.getByTestId("dummy-div");
 
-    expect(screen.getByTestId("dummy-div")).toBeInTheDocument();
-    expect(ref.current).not.toEqual({ current: null });
+    expect(scrollableDiv).toBeInTheDocument();
+    expect(ref.current).not.toEqual(null);
+    expect(ref.current).toHaveAttribute("data-testid", "dummy-div");
     expect(ref.current).toBeInstanceOf(HTMLDivElement);
-    expect(ref.current).toHaveStyle({
-      boxShadow: undefined,
-    });
   });
-
-  it.fails("box shadow applied correctly", async () => {
-    const { result } = renderHook(() => useScrollShadow());
-    const { ref } = result.current;
-
-    render(<DummyComponentWithChildren ref={ref} />);
-
-    const styledElement = screen.getByTestId("dummy-div");
-
-    const computedStyles = window.getComputedStyle(styledElement);
-    expect(computedStyles.boxShadow).toBe(bottomShadow);
-  });
-
-  // it("box shadow applied correctly when scrolled", () => {
-  //   const { result } = renderHook(() => useScrollShadow());
-  //   const { ref } = result.current;
-
-  //   render(
-  //     <Box
-  //       ref={ref}
-  //       data-testid="dummy-div"
-  //       style={{ height: "100px", overflowY: "auto" }}
-  //     />
-  //   );
-
-  //   if (ref.current) {
-  //     ref.current.scrollTop = 10;
-  //     ref.current.scrollHeight = 100;
-  //     ref.current.clientHeight = 90;
-  //     ref.current.onscroll(new Event("scroll"));
-  //   }
-
-  //   expect(ref.current.style.boxShadow).toBe(
-  //     "inset 0 -8px 5px -5px rgb(200 200 200 / 1)"
-  //   );
-  // });
 });
