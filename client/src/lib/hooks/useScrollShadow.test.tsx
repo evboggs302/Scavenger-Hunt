75f9fa43666176c9d169d42a3cbaf62956c16410
evboggs302 <evboggs94@gmail.com>
2025-07-25 16:31:05 -0400

    confirm test run

--- client/src/lib/hooks/useScrollShadow.test.tsx
+++ client/src/lib/hooks/useScrollShadow.test.tsx
@@ -19,20 +19,29 @@ const DummyComponent = forwardRef<HTMLElement>((_, ref) => (
 ));
 
 describe("useScrollShadow", () => {
-  vi.stubGlobal(
-    "ResizeObserver",
-    class MockedResizeObserver {
-      constructor(cb: ResizeObserverCallback) {
-        setTimeout(() => {
-          cb([], this);
-        }, 100);
-      }
+  const disconnectSpy = vi.fn();
 
-      observe = vi.fn();
-      unobserve = vi.fn();
-      disconnect = vi.fn();
-    }
-  );
+  beforeEach(() => {
+    vi.stubGlobal(
+      "ResizeObserver",
+      class MockedResizeObserver {
+        constructor(cb: ResizeObserverCallback) {
+          setTimeout(() => {
+            cb([], this);
+          }, 100);
+        }
+
+        observe = vi.fn();
+        unobserve = vi.fn();
+        disconnect = disconnectSpy;
+      }
+    );
+  });
+
+  afterEach(() => {
+    disconnectSpy.mockClear();
+    vi.unstubAllGlobals();
+  });
 
   it("hook default state", async () => {
     const { result } = renderHook(() => useScrollShadow());
@@ -53,5 +62,18 @@ describe("useScrollShadow", () => {
     expect(ref.current).not.toEqual(null);
     expect(ref.current).toHaveAttribute("data-testid", "dummy-div");
     expect(ref.current).toBeInstanceOf(HTMLDivElement);
+  });
+
+  it("ResizeObserver disconnects", async () => {
+    const { result } = renderHook(() => useScrollShadow());
+    const { ref } = result.current;
+
+    const { unmount } = await waitFor(() =>
+      render(<DummyComponent ref={ref} />)
+    );
+
+    unmount();
+
+    expect(disconnectSpy).toBeCalled();
   });
 });
